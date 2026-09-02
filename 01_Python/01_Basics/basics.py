055b867e2b6c3ddc175d5e3ae48e771a9a9ebb18
RajanTembhare <tembharerajan459@gmail.com>
2026-09-02 18:53:41 +0530

    update

--- 01_Python/01_Basics/basics.py
+++ 01_Python/01_Basics/basics.py
@@ -1,1 +1,14 @@
-print("hello")
+#IN python there is a lot of variables and functions and datatypes but ,i just study the important all the things in python ,thats is promise to myself
+ #datatypes
+ #variables
+ #operators
+ # conditional statemnt
+ #functions
+ #lists
+ #tuples
+ #sets
+ #dictionaries
+ #file handeling
+ #oops
+ # and all the things
+print("rajan tembhare")
