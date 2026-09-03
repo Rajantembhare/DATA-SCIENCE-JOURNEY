53f072188468b91a24da62e5d083710147ca3c06
RajanTembhare <tembharerajan459@gmail.com>
2026-09-03 22:29:13 +0530

    update

---
+++ 01_Python/03_Data_Types/data_types.py
@@ -0,0 +1,18 @@
+# the data type are used to described the data types ,means data is in which form ,like it is 
+# character
+#integer
+#float
+#numeric
+#etc
+
+
+# So now i just try the ,and use the all data types in python
+#integer
+#it is used when the data is in integer form -
+a=int(input("my name is rajan"))
+b=int(input("i am a single not in relationship"))
+print(b)
+ 
+
+
+
