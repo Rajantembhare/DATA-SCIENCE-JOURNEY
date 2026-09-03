6e418215323de9009b0dca8b05ca4bab6de408f6
RajanTembhare <tembharerajan459@gmail.com>
2026-09-03 21:31:41 +0530

    update

---
+++ 01_Python/02_Variables/variables.py
@@ -0,0 +1,20 @@
+# so the variable is used to store the data for a time ,variabke is very important in any programming language 
+a="rajan tembhare"
+b=23
+c=23.21
+age=21
+name="rajan"
+print(b)# we can print all the variable ,the data will be print where the data is store inside the variable
+print(age)
+#for example 
+a=12
+b=15
+a=b
+print(a)# we can also change the data from one variable to another variable ,it is also used in swapping the numbers 
+#next example
+a=15
+b=35
+c=a+b
+print(c)
+sum=c+a
+print(sum)
