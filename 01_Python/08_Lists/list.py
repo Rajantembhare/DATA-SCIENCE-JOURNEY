fa647cc115d113ff2e15b8eafd6b525a9041f990
RajanTembhare <tembharerajan459@gmail.com>
2026-09-04 22:05:27 +0530

    update

---
+++ 01_Python/08_Lists/list.py
@@ -0,0 +1,7 @@
+#list python ka bahot sahi topic chalo isse mai real life se jodne ki kosis karta hu agar 
+#mere ghar pe shop se saman laane ko kaha tab mai ek paper pe kya lana hai o likh dunga hai o shop se kharid lunga 
+#usi tarah yeh bhi list ki tarah use hota hai 
+#fir o kisi bhi chij kki list ho ise 
+#hum example se samajhate hai
+car={"bmw","tata","suzuki","mg","toyoto"}
+print(car)
