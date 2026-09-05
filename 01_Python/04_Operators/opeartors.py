92261af753fdcfc9c9d1f1cf22d9bb3d49c2458a
RajanTembhare <tembharerajan459@gmail.com>
2026-09-05 11:10:33 +0530

    update

---
+++ 01_Python/04_Operators/opeartors.py
@@ -0,0 +1,34 @@
+#operators
+#there is a some operators which help you to solve the opertions in python and othe language 
+#1. Arithmetic	+ - * / % // **	10 + 5
+#2. Comparison	== != > < >= <=	10 > 5
+#3. Logical	and or not	a > 5 and b < 10
+#4. Assignment	= += -= *= /= %= //= **=	x += 5
+#5. Bitwise	& | ^ ~ << >>	5 & 3
+#6. Membership	in, not in	'a' in 'apple'
+#7. Identity	is, is not	a is b
+ #so now i just solve the problems
+
+a=25
+b=25
+c=a+b
+print(c)#arithematic operators
+
+
+a=70
+b=70
+if a==b:
+    print("it is equals to")
+else:
+    print("it is not equals to")#comparison operators
+    
+    
+    
+a=12
+b=35
+if a<b:#Logical value
+    print("b is greater value")
+else:
+    print("a is greater value")
+    
+ 
