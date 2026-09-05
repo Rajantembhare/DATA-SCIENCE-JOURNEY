37c63369cfb92c4d2a2ff39d49cea28f4eb9a456
RajanTembhare <tembharerajan459@gmail.com>
2026-09-05 11:18:22 +0530

    update

---
+++ 01_Python/07_Functions/functions.py
@@ -0,0 +1,24 @@
+#function yeh koi bhi coding language ka sabse important topic hai ,iska use sabse jyada 
+#kiya jaata ,agar hme koi bhi program baar baar execute karna bina dubara firse code likhe tab 
+#function ka use hota hai 
+#koi bhi function ka naam reh sakta hai bus uske aage duble bracket dena pdta hai
+#1.remote()
+#2.system()
+#hum apne hisaab se function bna sakte hai
+def greet():
+    def greet():
+        print("Hello, Welcome to Python!")
+        
+   
+
+greet()   # function call
+#aaise likhte hai
+#bus mujhe dhyan rehne chahiye
+#format explain karte hai
+#pehle function ko bnao def greet()
+#fir uske under kya hme print karna hai ya kya logic lgana hai
+print("Hello, Welcome to Python!")
+#fir fuctio ko call karna pdenga
+#uske naam se ki bhai execute hoja kya tere under execution hai o
+greet()
+#isase
