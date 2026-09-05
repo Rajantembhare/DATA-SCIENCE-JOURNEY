fa70f7e37d6aac97849e8e7979adc5e08e5ceac7
RajanTembhare <tembharerajan459@gmail.com>
2026-09-05 11:15:20 +0530

    update

---
+++ 01_Python/05_Conditional_Statements/conditional.py
@@ -0,0 +1,36 @@
+# is topic me  ,yeh hume problem me rehne wale condition ke waqt use karna pdta hai
+#waise toh condition ko lgane me 
+#if agar agar do condition ek saath lgaana hai tab ,hum if else ka use karte hai
+#1.if -else
+#2.if -else ladder
+#3.if
+#4.if (if()else())else()
+#5.if elif() else()
+#and other 
+#abhi agar hme condition me aaisi chij puchi ho ki agar pehli  condition possible ho rahi hai toh 
+#print karo ,aur agar o condition satisfied nhi ho rhi hai tab next second one condition 
+#ko print karo ,aise logic se conditional statement use hote hai 
+#for example,
+x = 3
+if x % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+    
+#for example,
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+#kabhi kabhi multiple condition me bhi use hote hai,jaise ki if,elif,elif,elif,else like
+
+    
+
+
+
