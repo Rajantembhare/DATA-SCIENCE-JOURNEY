d254450869d8818e20ff8e7bfd4a7812743dae22
RajanTembhare <tembharerajan459@gmail.com>
2026-09-06 12:53:52 +0530

    update

---
+++ 01_Python/12_Strings/string.py
@@ -0,0 +1,14 @@
+#Text is a string data type. Any data type written as text is a string. Any data under single,
+# double or triple quote are strings. There are different string methods and built-in functions to
+# deal with string data types. To check the length of a string use the len() method.
+
+#Creating a String
+
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be made using a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I hope you are enjoying 30 days of Python Challenge"
+print(sentence)
