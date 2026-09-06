cd3b467c6d456bdfaa112f777bb2b057d15d2474
RajanTembhare <tembharerajan459@gmail.com>
2026-09-06 12:52:27 +0530

    update

---
+++ 01_Python/11_Dictionaries/dictionary.py
@@ -0,0 +1,42 @@
+# dictionary is like a all the information is stores ,dictionary me rehne wale chijo ke andar 
+#kya hai o bhi stores kar sakte hai,aur kaafi sahi hai
+
+
+
+dictionary={
+    "name":"rajan",
+    "adreess":"mp",
+    "nickname":"noor",
+    "surname":"tembhare",
+    "dream":"khush_rehna",
+    "girlfriend":"no one",
+
+    
+    
+}
+print(dictionary)
+ 
+ 
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+print(person['first_name']) # Asabeneh
+print(person['country'])    # Finland
+print(person['skills'])     # ['JavaScript', 'React', 'Node', 'MongoDB', 'Python']
+print(person['skills'][0])  # JavaScript
+print(person['address']['street']) # Space street
+#print(person['city'])       # Error
+
+
+
+
+
