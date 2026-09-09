b080b1672da1a2abd0faa5f69962cc41ba8af05a
RajanTembhare <tembharerajan459@gmail.com>
2026-09-10 02:45:20 +0530

    update

---
+++ 01_Python/16_modules/modules.py
@@ -0,0 +1,14 @@
+# module is used to code in multiple times ,like code reusability
+#A module is simply a file that contains Python code (functions, classes, or variables) which
+# you can reuse in other programs.
+#It helps in organizing code into smaller, manageable parts.
+#Promotes reusability and maintainability.
+#Python itself comes with many built-in modules (like math, os, random).
+#You can also create your own custom module
+import math
+
+# Using math module functions
+radius = 7
+area = math.pi * math.pow(radius, 2)
+
+print("Area of Circle =", area)
