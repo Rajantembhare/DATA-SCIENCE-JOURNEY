f1e2afd43fa2fe657decccc4ed518d46a9ba6dd4
RajanTembhare <tembharerajan459@gmail.com>
2026-09-10 13:19:08 +0530

    update

---
+++ 01_Python/07_Functions/01.problem.py
@@ -0,0 +1,21 @@
+#def remote():
+    #a=int(input("enter the first number:"))
+      #c=int(input("enter the third number:"))
+    #d=int(input("enter the fourth number:"))
+    
+    #avg=(a+b+c+d)/4
+    #print("average:",avg)
+    #remote()
+    # """ 
+def remote():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    c = int(input("Enter the third number: "))
+    d = int(input("Enter the fourth number: "))
+    
+    avg = (a + b + c + d) / 4
+    print("Average =", avg)
+
+# Call function once
+remote()
+remote()
